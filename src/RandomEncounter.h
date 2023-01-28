95c76b61fd561c939a28a0b5872e25521274bc84
luijavi <luijavi@gmail.com>
2023-01-28 13:52:24 -0600

    Rename TurnType to EncounterTrigger to be more descriptive.

--- src/RandomEncounter.h
+++ src/RandomEncounter.h
@@ -3,26 +3,14 @@
 #include <string>
 #include <vector>
 
-//enum class ImpactType { kNone, kIncrease, kDecrease };
-//
-//struct Effects
-//{
-//	int cash = 0;
-//	int health = 0;
-//	bool inventory_flower = false;
-//	bool inventory_gun = false;
-//	bool market_price = false;
-//	bool other = false;
-//};
-
-enum class TurnType { kNextDay, kFlyAway, kBoth };
+enum class EncounterTrigger { kNextDay, kFlyAway, kBoth };
 
 class RandomEncounter
 {
 public:
 	RandomEncounter(const std::string& encounter_data);
 private:
-	TurnType m_turn_type;
+	EncounterTrigger m_turn_type;
 	unsigned short m_probability;
 	std::vector<std::string> m_effects;
 	std::string m_text;
