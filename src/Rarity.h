f9e3479dc91daabd0a239bb1a6fed99d43111540
luijavi <luijavi@gmail.com>
2023-02-18 14:09:04 -0600

    Add Rarity header (moved structs out of Flower.h)

---
+++ src/Rarity.h
@@ -0,0 +1,22 @@
+#pragma once
+
+enum class Rarity
+{
+	kInvalid,
+	kCommon,
+	kUncommon,
+	kRare,
+	kLegendary,
+	kMythic,
+	kUnique
+};
+
+// Used to set the price and quantities at market
+struct RarityValues
+{
+	Rarity rarity_key{ Rarity::kInvalid };
+	double min_price{ 0.0 };
+	double max_price{ 0.0 };
+	int min_quantity{ 0 };
+	int max_quantity{ 0 };
+};
